37aabd7287a486f632a510a7ced82bfa878ec78d
johnnychang889 <johnnychang889@gmail.com>
2022-02-12 19:52:50 +0800

    update pause window

--- Scripts/pausePlayControl.cs
+++ Scripts/pausePlayControl.cs
@@ -5,14 +5,11 @@ using UnityEngine.UI;
 
 public class pausePlayControl : MonoBehaviour
 {
-    Image pausePlayImage;
-    public Sprite pauseButton;
-    public Sprite playButton;
     public GameObject PauseWindow;
     private bool isPause = false;
     void Start()
     {
-        pausePlayImage = GetComponent<Image>();
+        
     }
 
     // Update is called once per frame
@@ -26,14 +23,12 @@ public class pausePlayControl : MonoBehaviour
         isPause = !isPause;
 
         if (isPause == true)
-        {;
-            pausePlayImage.sprite = playButton;
+        {
             PauseWindow.gameObject.SetActive(true);
             Time.timeScale = 0;
         }
         else
         {
-            pausePlayImage.sprite = pauseButton;
             PauseWindow.gameObject.SetActive(false);
             Time.timeScale = 1;
         }
